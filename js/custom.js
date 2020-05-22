3b8de5b853b8aa1ef37897eba17af8908b244928
hammadyounas <m.hammadyounas@outlook.com>
2020-05-23 00:11:55 +0500

    changs done

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,22 @@ $('.order_summary_open').click(function() {
     $('.order-summary-container').fadeIn();
 });
 
+$('.order_summary_open2').click(function() {
+    $('.order-summary-container').fadeOut();
+    $('.order-summary-container2').fadeIn();
+});
+
+
+
 $('.order_summary_close').click(function() {
     $('.order-summary-container').fadeOut();
 });
+
+$('.order_summary_close2').click(function() {
+    $('.order-summary-container2').fadeOut();
+});
+
+
 
 $('.navbar_image').click(function() {
     $('.mini-profile').toggleClass('show');
@@ -14,4 +27,13 @@ $('.navbar_image').click(function() {
 
 $('.all-tools-menu').click(function() {
     $('.tool-menu').toggleClass('show');
+});
+
+$('#side-menu-btn').click(function(){
+    $('.sidebar').toggleClass('show');
+});
+
+$('.profile-close-btn').click(function() {
+    $('.mini-profile').hide();
+
 });
